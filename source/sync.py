4c0c0727f6e5de264d009f3335b47c295b15dd16
agent87 <arnauldkayonga1@gmail.com>
2020-04-08 23:41:34 +0200

    added local sync

--- source/sync.py
+++ source/sync.py
@@ -1,9 +1,18 @@
-class sync:
-    def local_(self):
+from dirsync import sync
+
+
+
+class synchronise():
+    def local_(garmin, back_folder_name): #Include Garmin watch mount point, back-up folder location
         print("this is the local sync function")
+        sync(garmin, back_folder_name, 'sync', purge = True)
 
     def cloud_(self):
         print("This is the cloud sync function")
     
     def rasp_(self):
         print("This is the raspberyy sync function")
+
+
+
+synchronise.local_("/media/sangman/GARMIN","~/Desktop/archive/garmin")
