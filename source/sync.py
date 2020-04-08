9c4b272bbe9239771f9b0fa7cabef8efa80d230b
agent87 <arnauldkayonga1@gmail.com>
2020-04-08 12:20:58 +0200

    local file sync

--- source/sync.py
+++ source/sync.py
@@ -2,8 +2,7 @@ import pyudev
 
 watch_name = "GARMIN"
 context = pyudev.Context()
-
-
+forra = context.list_devices()
 def port_scan(watch_name):
     monitor = pyudev.Monitor.from_netlink(context)
     monitor.filter_by('block')
@@ -11,8 +10,20 @@ def port_scan(watch_name):
         if device.action == "add":
             if device.get('ID_FS_LABEL') == watch_name :
                 print("Watch detected")
+                return True
         elif device.action == "remove":
             if device.get('ID_FS_LABEL') == watch_name :
                 print("Watch Removed!")
+                break
 
-port_scan(watch_name)
+
+def local_sync():
+    print("this is the local sync")
+
+def cloud_sync():
+    print("This is the cloud sync")
+
+
+
+state = port_scan(watch_name)
+print(state)
