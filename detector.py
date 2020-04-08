5df66912827b15c2e1348e8a41f43d763c054857
agent87 <arnauldkayonga1@gmail.com>
2020-04-08 23:21:20 +0200

    added asynchronousport scan function

--- detector.py
+++ detector.py
@@ -1,34 +1,39 @@
 import pyudev
-from source.sync import *
+import psutil
+#from source.sync import *
 import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-secret_key = config['DEFAULT']['SECRET_KEY'] # 'secret-key-of-myapp'
-ci_hook_url = config['CI']['HOOK_URL'] # 'web-hooking-url-from-ci-service'
 
-watch_name = "GARMIN"
-context = pyudev.Context()
-forra = context.list_devices()
-def port_scan(watch_name):
+
+
+def port_scan():
+    context = pyudev.Context()
     monitor = pyudev.Monitor.from_netlink(context)
     monitor.filter_by('block')
     for device in iter(monitor.poll, None):
+        print(device.device_node)
         if device.action == "add":
-            if device.get('ID_FS_LABEL') == watch_name :
+            if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL'] :
+                for p in psutil.disk_partitions():
+                    print(' ')#p.device)# in device['DEVNAME']:
+                    #    print("  {}: {}".format(p.device, p.mountpoint))
                 print("Watch detected")
-                return True
         elif device.action == "remove":
-            if device.get('ID_FS_LABEL') == watch_name :
+            if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL'] :
                 print("Watch Removed!")
                 break
 
 
+def async_scan():
+    context = pyudev.Context()
+    for device in context.list_devices(subsystem='block'):
+        if device.device_node in psutil.disk_partitions():
+            print("YES")
 
 
-if port_scan(watch_name) == True:
-    sync.cloud_(watch_name)
-
+async_scan()
 
 
