ab1272836dab22cc03a107cd35f7cdb43aebbea3
agent87 <arnauldkayonga1@gmail.com>
2020-04-09 03:59:13 +0200

    test 1.2

--- detector.py
+++ detector.py
@@ -1,49 +1,45 @@
 import pyudev
 import psutil
 import time
-#from source.sync import *
 import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 
-
-
-def port_scan():
-    context = pyudev.Context()
-    monitor = pyudev.Monitor.from_netlink(context)
-    monitor.filter_by('block')
-    for device in iter(monitor.poll, None):
-        if device.action == "add":
-            if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL']:
-                return device.device_node
-        elif device.action == "remove":
-            if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL']:
-                print("Watch Removed!")    #remove this after debug
-                
-
-
-def async_scan():
+def async_scan():   #asynchronous port scan
     context = pyudev.Context()
     for device in context.list_devices(subsystem='block'):
         if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL']:
             return device.device_node
         else:
             break
-       
 
+def sync_scan():   #synchronous port scan
+    context = pyudev.Context()
+    monitor = pyudev.Monitor.from_netlink(context)
+    monitor.filter_by('block')
+    for device in iter(monitor.poll, None):
+        if device.action == "add":
+            if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL']:
+                print("SHOW")
+                return async_scan()
+        elif device.action == "remove":
+            if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL']:
+                print("Watch Removed!")    #remove this after debug
+                break
+                
 def mount_point(device_node):
-    print(psutil.disk_partitions())
     for partitions in psutil.disk_partitions():
         if partitions.device == device_node:
+            partitions.refreshDiskInfo()
             return partitions.mountpoint
 
 
-#mount_point(async_scan())
+
+
 if async_scan() == None:
-    time.sleep(1)
-    print(mount_point(port_scan()))
+    print(mount_point(sync_scan()))
 else:
     print(mount_point(async_scan()))
     
