eb612974a4cb73d1aa3aa24c63028dcd9be06867
agent87 <arnauldkayonga1@gmail.com>
2020-04-10 01:02:42 +0200

    Async usb port scan

--- detector.py
+++ detector.py
@@ -1,4 +1,5 @@
 import pyudev
+from pyudev import Context, Monitor, MonitorObserver
 import psutil
 import time
 import configparser
@@ -6,12 +7,16 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+def mount_point(device_node):
+    for partitions in psutil.disk_partitions():
+        if partitions.device == device_node:
+            return partitions.mountpoint
 
 def async_scan():   #asynchronous port scan
     context = pyudev.Context()
     for device in context.list_devices(subsystem='block'):
         if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL']:
-            return device.device_node
+            return mount_point(device.device_node)
         else:
             break
 
@@ -22,24 +27,29 @@ def sync_scan():   #synchronous port scan
     for device in iter(monitor.poll, None):
         if device.action == "add":
             if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL']:
-                print("SHOW")
-                return async_scan()
+                print(mount_point(device.device_node))
         elif device.action == "remove":
             if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL']:
                 print("Watch Removed!")    #remove this after debug
-                break
-                
-def mount_point(device_node):
-    for partitions in psutil.disk_partitions():
-        if partitions.device == device_node:
-            partitions.refreshDiskInfo()
-            return partitions.mountpoint
+
+
+def print_device_event(device):
+    print('background event {0.action}: {0.device_path}'.format(device)) 
 
 
 
+context = Context()
+monitor = Monitor.from_netlink(context)
+monitor.start()
+monitor.filter_by(subsystem='block')
+for action, device in monitor:
+    print('{0}: {1}'.format(action, device.device_node))
+    print(mount_point(device.device_node))
 
-if async_scan() == None:
-    print(mount_point(sync_scan()))
-else:
-    print(mount_point(async_scan()))
+
+#if async_scan() == None:
+#holla = mount_point(sync_scan())
+#print(holla)
+#else:
+#    print(mount_point(async_scan()))
     
