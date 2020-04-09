3733005ac809bca6351f7e8199678e9561bfa312
agent87 <arnauldkayonga1@gmail.com>
2020-04-09 02:41:17 +0200

    added mountpoint function

--- detector.py
+++ detector.py
@@ -1,5 +1,6 @@
 import pyudev
 import psutil
+import time
 #from source.sync import *
 import configparser
 
@@ -14,26 +15,35 @@ def port_scan():
     monitor = pyudev.Monitor.from_netlink(context)
     monitor.filter_by('block')
     for device in iter(monitor.poll, None):
-        print(device.device_node)
         if device.action == "add":
-            if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL'] :
-                for p in psutil.disk_partitions():
-                    print(' ')#p.device)# in device['DEVNAME']:
-                    #    print("  {}: {}".format(p.device, p.mountpoint))
-                print("Watch detected")
+            if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL']:
+                return device.device_node
         elif device.action == "remove":
-            if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL'] :
-                print("Watch Removed!")
-                break
+            if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL']:
+                print("Watch Removed!")    #remove this after debug
+                
 
 
 def async_scan():
     context = pyudev.Context()
     for device in context.list_devices(subsystem='block'):
-        if device.device_node in psutil.disk_partitions():
-            print("YES")
+        if device.get('ID_FS_LABEL') == config['credential']['ID_FS_LABEL']:
+            return device.device_node
+        else:
+            break
+       
+
+def mount_point(device_node):
+    print(psutil.disk_partitions())
+    for partitions in psutil.disk_partitions():
+        if partitions.device == device_node:
+            return partitions.mountpoint
 
 
-async_scan()
-
-
+#mount_point(async_scan())
+if async_scan() == None:
+    time.sleep(1)
+    print(mount_point(port_scan()))
+else:
+    print(mount_point(async_scan()))
+    
