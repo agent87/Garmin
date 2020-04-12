541f506a9a27fba34aeb0748c8ce030c26228f61
agent87 <arnauldkayonga1@gmail.com>
2020-04-12 21:00:38 +0200

    Added pyparser

--- garmin.py
+++ garmin.py
@@ -1,4 +1,14 @@
-from fitdecode import *
-from fitparser import *
+from fitparse import FitFile
 
 
+fitfile = FitFile('data/ACTIVITY/A2OJ1736.FIT')
+
+# Get all data messages that are of type record
+for record in fitfile.get_messages('unknown_22'):#'record'):
+    # Go through all the data entries in this record
+    for record_data in record:
+        # Print the records name and value (and units if it has any)
+        if record_data.units:
+            print(" * {}: {} {}".format(record_data.name, record_data.value, record_data.units))
+        else:
+            print(" * {}: {}".format(record_data.name, record_data.value))
