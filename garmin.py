c7bb5f0c108627b2df6f765c66cf39013dde3a44
agent87 <arnauldkayonga1@gmail.com>
2020-04-12 23:18:45 +0200

    added pandas

--- garmin.py
+++ garmin.py
@@ -1,14 +1,29 @@
-from fitparse import FitFile
+from fitparse import *
+from pandas import *
 
 
-fitfile = FitFile('data/ACTIVITY/A2OJ1736.FIT')
 
-# Get all data messages that are of type record
-for record in fitfile.get_messages('unknown_22'):#'record'):
+try:
+    fitfile = FitFile('data/raw/ACTIVITY/A2OJ1736.FIT')
+    fitfile.parse()
+except FitParseError as e:
+    print("Error while parsing .FIT file: %s" % e)
+    sys.exit(1)
+
+df = DataFrame()
+
+
+for record in fitfile.get_messages():#'record'):
+    df
+    print(record.name)
     # Go through all the data entries in this record
-    for record_data in record:
+    #for record_data in record:
+        #print(count)
+        #count += 1
         # Print the records name and value (and units if it has any)
-        if record_data.units:
-            print(" * {}: {} {}".format(record_data.name, record_data.value, record_data.units))
-        else:
-            print(" * {}: {}".format(record_data.name, record_data.value))
+        #if record_data.units:
+            #print(" * {}: {} {}".format(record_data.name, record_data.value, record_data.units))
+        #else:
+            #print(" * {}: {}".format(record_data.name, record_data.value))
+
+            #'''
