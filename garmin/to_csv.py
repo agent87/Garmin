436bfa48d216247014384413c4d709c9bc324606
agent87 <arnauldkayonga1@gmail.com>
2020-04-13 19:39:44 +0200

    Added Mass export function

--- garmin/to_csv.py
+++ garmin/to_csv.py
@@ -6,17 +6,17 @@ import ntpath
 
 class compile_list:
     #Returns a list of .FIT files that haven't already been converted using history log.txt file
-    def find(pattern, path):
+    def find():
         result = []
-        for root, dirs, files in os.walk(path):
+        for root, dirs, files in os.walk("../data/raw"):
             for name in files:
-                if fnmatch.fnmatch(name, pattern):
+                if fnmatch.fnmatch(name, "*.FIT"):
                     result.append(os.path.join(root, name))
         return result 
 
-    def to_convert(path, find):
+    def to_convert(find):
         to_convert = []
-        already_csv = open(path,'r').read().splitlines()
+        already_csv = open("../data/converted/log.txt",'r').read().splitlines()
         for fit_file_path in find:
             if ntpath.basename(fit_file_path) in already_csv:
                 pass
@@ -25,11 +25,12 @@ class compile_list:
         return to_convert
 
 
+
 class export_csv:
     def output_path(filepath):
-        path = r"{}".format(filepath[0]).split("/")
+        path = r"{}".format(filepath).split("/")
         path[path.index("raw")] = "converted"
-        return "/".join(path)
+        return "/".join(path).replace('FIT', 'csv')
 
     def update_log(filename):
         #update logs files
@@ -50,9 +51,9 @@ class export_csv:
     def df_with_headers(list_headers, dataframe): 
         #Creates a Dataframe and enlists all the headers
         for header in list_headers:
-            if header in df.columns:
+            if header in dataframe.columns:
                 pass
-            elif header is not df.count:
+            elif header is not dataframe.count:
                 dataframe[header] = None
         return 
     
@@ -65,21 +66,37 @@ class export_csv:
                 dataframe.loc[count, record_data.name] = record_data.value
             count += 1
 
-    def export(filepath, output_path):
+    def s_export(filepath):
+        #single_ binary fit file convertion
+        df = DataFrame()
         try:
             fitfile = FitFile(filepath)
             fitfile.parse()
         except FitParseError as e:
             print("Error while parsing .FIT file: %s".format(e))
             sys.exit(1)
-        df = DataFrame()
-        export_csv.df_with_headers(export_csv.list_headers(fitfile), df)
+        export_csv.df_with_headers(export_csv.list_headers(fitfile), df)  #Include the headers in Dataframe
         export_csv.data_entry(export_csv.list_headers(fitfile), df, fitfile)
-        df.to_csv(output_path)
-        export_csv.update_log()
-        
-        
+        df.to_csv(export_csv.output_path(filepath))
+        export_csv.update_log(ntpath.basename(filepath))
+
+    def mass_export():
+        for files in compile_list.to_convert(compile_list.find()):
+            export_csv.s_export(files)
+            print("Exporting the {} file!".format(ntpath.basename(files)))
+
+#export_csv.s_export("../data/raw/MONITOR/A3LD3812.FIT")
+#print(export_csv.output_path("../data/raw/MONITOR/A3LD3812.FIT"))
+export_csv.mass_export()
+
+'''        
 if __name__ == "__main__":
+
     #l = compile_list.to_convert("../data/converted/log.txt", compile_list.find("*.FIT","../data/raw"))
     #export_csv.update_log("BLABLA")
-    print(export_csv.output_path(compile_list.to_convert("../data/converted/log.txt", compile_list.find("*.FIT","../data/raw"))))
+    #print(export_csv.output_path(compile_list.to_convert("../data/converted/log.txt", compile_list.find("*.FIT","../data/raw"))))
+    #for file_path in compile_list.to_convert("../data/converted/log.txt", compile_list.find("*.FIT","../data/raw")):
+    #    print(file_path)
+        
+    #    export(file_path, export_csv.output_path(file_path))
+'''
