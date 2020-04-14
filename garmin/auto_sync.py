0a4fb95ffb4949d301c2e7ae5a4e3bdbdd180c5d
agent87 <arnauldkayonga1@gmail.com>
2020-04-14 23:57:24 +0200

    Added nohup call

---
+++ garmin/auto_sync.py
@@ -0,0 +1,24 @@
+
+from pyudev import Context, Monitor, MonitorObserver
+import pyudev
+import time
+import configparser
+from subprocess import Popen, PIPE
+import os
+from sync import synchronise
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+context = pyudev.Context()
+monitor = pyudev.Monitor.from_netlink(context)
+monitor.filter_by('block')
+for device in iter(monitor.poll, None):
+    if config['credential']['ID_FS_LABEL'] == device.get('ID_FS_LABEL'):
+        if device.action == "add":
+            time.sleep(1)
+            garmin_path = os.popen('lsblk -n {} -p -o MOUNTPOINT'.format(device.device_node)).read().strip('\n') 
+            synchronise.local_(garmin_path, "../data/raw/")
+        else:
+            pass
+
